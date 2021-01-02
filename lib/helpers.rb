a92dcbfba8277e9179b9c53e75293a9feaa5a41a
Ukaza Perdana <ukaza@hey.com>
2021-01-02 16:31:17 +0800

    fix responsiveness

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,3 +1,5 @@
+require 'digest'
+
 use_helper Nanoc::Helpers::Rendering
 use_helper Nanoc::Helpers::Blogging
 
@@ -6,15 +8,14 @@ def format_date(date)
 end
 
 # Fingerprinting for cache busting
-
-FINGERPRINTS ||= Hash.new do |h, key|
-  # Need to generate the fingerprint based on the actual hash of the file, but this will do for now.
-  h[key] = SecureRandom.alphanumeric(13)
+def calculate_fingerprint(filename)
+  sha256 = Digest::SHA256.file File.join("content", filename)
+  sha256.hexdigest
 end
 
 def fingerprinted_name(identifier)
-  fp = FINGERPRINTS[identifier]
+  fp = calculate_fingerprint(identifier)
   name_parts = identifier.split(".")
-  name_parts[-1] = "#{fp}.#{name_parts[-1]}"
+  name_parts[-2] = "#{name_parts[-2]}-#{fp}"
   name_parts.join(".")
 end
