1460245536c7b9fe874c131e7971f890df6af98d
Ukaza Perdana <ukaza@hey.com>
2021-01-30 16:20:10 +0800

    Fix cache busting on live preview

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -4,18 +4,26 @@ use_helper Nanoc::Helpers::Rendering
 use_helper Nanoc::Helpers::Blogging
 
 def format_date(date)
-  Date.parse(date).strftime('%d %b %Y')
+  if date.respond_to? :strftime
+    date.strftime('%d %b %Y')
+  else
+    Date.parse(date).strftime('%d %b %Y')
+  end
 end
 
 # Fingerprinting for cache busting
-def calculate_fingerprint(filename)
-  sha256 = Digest::SHA256.file File.join("content", filename)
-  sha256.hexdigest
-end
-
+# https://gist.github.com/ddfreyne/ef87bc498ec9639b0102d108af92c96c
 def fingerprinted_name(identifier)
   fp = calculate_fingerprint(identifier)
   name_parts = identifier.split(".")
   name_parts[-2] = "#{name_parts[-2]}-#{fp}"
   name_parts.join(".")
+end
+
+def calculate_fingerprint(pattern)
+  contents = @items.find_all(pattern).map do |item|
+    item.binary? ? File.read(item[:filename]) : item.raw_content
+  end
+
+  Digest::SHA1.hexdigest(contents.join(""))[0..15]
 end
